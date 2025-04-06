a84e230758ecd4abbd14def94940d2e46a7dac07
jaijal george <jaijalgeorge6@gmail.com>
2025-04-06 20:32:50 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Add any interactive scripts here
+
+// Example: Smooth scrolling for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
